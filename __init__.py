b373d7a4850fb2a7065bff196b7640e4ba435ea5
Paul Fariello <paul@fariello.eu>
2018-04-12 09:12:02 +0200

    Use retdec config file

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import os
 import re
 import tempfile
+import json
+import abc
 from subprocess import Popen, PIPE
 
 from pygments import highlight
@@ -8,75 +10,188 @@ from pygments.lexers import CLexer
 from pygments.formatters import HtmlFormatter
 from pygments.styles.native import NativeStyle
 
-from binaryninja.log import log_error
+from binaryninja import log
 from binaryninja.plugin import PluginCommand
 from binaryninja.interaction import show_message_box, show_html_report
 from binaryninja.enums import MessageBoxButtonSet, MessageBoxIcon
 
-ARCHS = ['x86', 'mips', 'powerpc', 'arm']
 BG_COLOR = '#272811'
+
+
+class ExceptionWithMessageBox(Exception):
+    def __init__(self, msg, info, icon=MessageBoxIcon.InformationIcon):
+        super(ExceptionWithMessageBox, self).__init__(msg)
+        show_message_box(msg, info, MessageBoxButtonSet.OKButtonSet, icon)
+
+
+class RetDecConfig(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def dump(self):
+        pass
+
+
+class RetDecFunctionConfig(RetDecConfig):
+    CALLING_CONVENTIONS = ["CC_UNKNOWN",
+                           "CC_VOIDARG",
+                           "CC_CDECL",
+                           "CC_ELLIPSIS",
+                           "CC_STDCALL",
+                           "CC_PASCAL",
+                           "CC_FASTCALL",
+                           "CC_THISCALL",
+                           "CC_MANUAL",
+                           "CC_SPOILED",
+                           "CC_SPECIALE",
+                           "CC_SPECIALP",
+                           "CC_SPECIAL"]
+
+    def __init__(self, start_addr):
+        self._conf = {'startAddr': start_addr}
+
+    @property
+    def calling_convention(self):
+        return self._conf['CallingConvention']
+
+    @calling_convention.setter
+    def calling_convention(self, calling_convention):
+        if calling_convention not in self.CALLING_CONVENTIONS:
+            msg = 'unsupported calling convention: {}'.format(calling_convention)
+            calling_conventions = ', '.join(self.CALLING_CONVENTIONS)
+            info = 'Only {} calling conventions are supported'.format(calling_conventions)
+            raise ExceptionWithMessageBox(msg, info)
+        self._conf['CallingConvention'] = calling_convention
+
+    def dump(self):
+        return self._conf
+
+
+class RetDecMainConfig(RetDecConfig):
+    ARCHS = ['x86', 'mips', 'powerpc', 'arm']
+    ENDIANNESS = ['big', 'little']
+
+    def __init__(self):
+        self._conf = {'architecture': {},
+                      'functions': []}
+
+    @property
+    def arch(self):
+        return self._conf['architecture']['name']
+
+    @arch.setter
+    def arch(self, arch):
+        if arch not in self.ARCHS:
+            msg = 'unsupported architecture: {}'.format(arch)
+            info = 'Only {} architectures are supported'.format(', '.join(self.ARCHS))
+            raise ExceptionWithMessageBox(msg, info)
+        self._conf['architecture']['name'] = arch
+
+    @property
+    def endianness(self):
+        return self._conf['architecture']['endianness']
+
+    @endianness.setter
+    def endianness(self, endianness):
+        if endianness not in self.ENDIANNESS:
+            msg = 'unsupported endianness: {}'.format(endianness)
+            info = 'Only {} endianness are supported'.format(', '.join(self.ENDIANNESS))
+            raise ExceptionWithMessageBox(msg, info)
+        self._conf['architecture']['endianness'] = endianness
+
+    def add_function(self, func):
+        """Add configuration of a function"""
+        self._conf['functions'].append(func)
+
+    def dump(self):
+        """Dump configuration into open file output in json format"""
+        conf = self._conf
+        conf['functions'] = [function.dump() for function in conf['functions']]
+        return conf
+
+
+class RetDecConfigFactory(object):
+    CC_TRANSLATE = {'unknown': "CC_UNKNOWN",
+                    'voidarg': "CC_VOIDARG",
+                    'cdecl': "CC_CDECL",
+                    'ellipsis': "CC_ELLIPSIS",
+                    'stdcall': "CC_STDCALL",
+                    'pascal': "CC_PASCAL",
+                    'fastcall': "CC_FASTCALL",
+                    'thiscall': "CC_THISCALL",
+                    'manual': "CC_MANUAL",
+                    'spoiled': "CC_SPOILED",
+                    'speciale': "CC_SPECIALE",
+                    'specialp': "CC_SPECIALP",
+                    'special': "CC_SPECIAL"}
+
+    @classmethod
+    def main(cls, view):
+        conf = RetDecMainConfig()
+        conf.arch = view.arch.name
+        conf.endianness = 'big' if view.endianness else 'little'
+        return conf
+
+    @classmethod
+    def func(cls, function):
+        conf = RetDecFunctionConfig(function.start)
+        conf.calling_convention = cls.CC_TRANSLATE[function.calling_convention.name]
+        return conf
+
 
 class RetDec(object):
     def __init__(self, view, function):
         self._view = view
         self._function = function
 
-        self.arch = self._view.arch.name
-        if self.arch not in ARCHS:
-            show_message_box('RetDec Offline Decompiler',
-                             'Only {} architectures are supported'.format(', '.join(ARCHS)),
-                             MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
-            raise Exception('unsupported architecture')
+        self.conf = RetDecConfigFactory.main(self._view)
+        self.conf.add_function(RetDecConfigFactory.func(self._function))
 
-        self.endianness = 'big' if self._view.endianness else 'little'
+        self._cmdline = ['retdec-decompiler.sh']
+        self._cmdline.append('--backend-no-debug-comments')
+        self._cmdline.append('--cleanup')
 
-        self.cmdline = ['retdec-decompiler.sh']
-        self.cmdline.append('-m')
-        self.cmdline.append('raw')
+    def decompile(self, inputfile):
+        with tempfile.NamedTemporaryFile(mode='w') as conf:
+            json.dump(self.conf.dump(), conf)
+            self._cmdline.extend(['--config', conf.name])
+            self._cmdline.append(inputfile)
+            log.log_info(" ".join(self._cmdline))
 
-        self.cmdline.append('-a')
-        self.cmdline.append(self.arch)
+            p = Popen(self._cmdline, stdout=PIPE, stderr=PIPE)
+            _, err = p.communicate()
+            if err.startswith('Error'):
+                raise ExceptionWithMessageBox('decompilation error', err)
 
-        self.cmdline.append('-e')
-        self.cmdline.append(self.endianness)
+            with open('{}.c'.format(inputfile), 'r') as f:
+                code = f.read()
 
-        self.cmdline.append('--backend-no-debug-comments')
-        self.cmdline.append('--cleanup')
+            os.unlink('{}.c'.format(inputfile))
+            os.unlink('{}.c.frontend.dsm'.format(inputfile))
+
+        return code
 
     def decompile_function(self):
-        filename = self.read_function()
+        self._cmdline.extend(['--mode', 'raw'])
+        self._cmdline.extend(['--raw-section-vma', '{:#x}'.format(self._function.start)])
+        self._cmdline.extend(['--raw-entry-point', '{:#x}'.format(self._function.start)])
+        self._cmdline.extend(['--arch', self.conf.arch])
+        self._cmdline.extend(['--endian', self.conf.endianness])
 
-        p = Popen(self.cmdline, stdout=PIPE, stderr=PIPE)
-        err = p.communicate()[1]
-        if err.startswith('Error'):
-            raise Exception(err)
+        with tempfile.NamedTemporaryFile('w+b') as f:
+            self.load_function(f)
 
-        with open('{}.c'.format(filename), 'r') as f:
-            code = f.read()
-
-        os.unlink('{}.c'.format(filename))
-        os.unlink('{}.c.frontend.dsm'.format(filename))
+            code = self.decompile(f.name)
 
         code = self.merge_symbols(code)
         self.render_output(code)
 
-    def read_function(self):
+    def load_function(self, output):
         start = self._function.start
         end = max([bb.end for bb in self._function.basic_blocks])
         raw = self._view.read(start, end - start)
 
-        fd, filename = tempfile.mkstemp()
-        with os.fdopen(fd, 'wb') as f:
-            f.write(raw)
-
-        self.cmdline.append('--raw-section-vma')
-        self.cmdline.append('{:#x}'.format(start))
-
-        self.cmdline.append('--raw-entry-point')
-        self.cmdline.append('{:#x}'.format(start))
-
-        self.cmdline.append(filename)
-        return filename
+        output.write(raw)
 
     def merge_symbols(self, code):
         pcode = []
@@ -122,6 +237,6 @@ def decompile(view, function):
         retdec = RetDec(view, function)
         retdec.decompile_function()
     except Exception as e:
-        log_error('failed to decompile function\n{}'.format(e.message))
+        log.log_error('failed to decompile function\n{}'.format(e.message))
 
 PluginCommand.register_for_function('RetDec Offline Decompiler', 'Decompile', decompile)
